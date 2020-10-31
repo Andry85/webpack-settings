3efc6ab3da0ccde8fc78838184fa9b08809eedd3
Andrii Shapoval <andrii.shapoval@smile-ukraine.com>
2020-10-31 23:16:15 +0200

    settings

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,2 @@
-import Print from './print.js';
 import '../sass/main.scss';
-import $ from 'jquery';
-import {b} from './modules/autorization';
-
-
-
-$('body').css('background-color', 'green');
-
-
-let a = 10;
-
-console.log(`I am: ${a + b}`);
-
-console.group( "process.env.NODE_ENV" );
-console.log( process.env.NODE_ENV );
-console.groupEnd();
+import Main from './main.js';
